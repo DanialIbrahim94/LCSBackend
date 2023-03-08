567e00272e65c7b96162b430a1a1ba6daa314507
Araari adem <araariadem0@gmail.com>
2023-03-08 12:45:30 +0100

    fix Business users cannot add New Account

--- deliverySys/settings/development.py
+++ deliverySys/settings/development.py
@@ -29,8 +29,8 @@ AUTH_PASSWORD_VALIDATORS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Email configuration
 
