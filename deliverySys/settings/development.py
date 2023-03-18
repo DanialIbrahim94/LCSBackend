2625c86db9bc50863f3e688b7ffe898788646037
Araari adem <araariadem0@gmail.com>
2023-03-18 14:36:14 +0100

    change coupons quantity from 1500 to 2500

--- deliverySys/settings/development.py
+++ deliverySys/settings/development.py
@@ -26,7 +26,7 @@ WOOCOMMERCE_PRODUCT_VARIATIONS = {
     250: config('WOOCOMMERCE_PRODUCT_VARIATIONS_250', cast=int),
     500: config('WOOCOMMERCE_PRODUCT_VARIATIONS_500', cast=int),
     1000: config('WOOCOMMERCE_PRODUCT_VARIATIONS_1000', cast=int),
-    1500: config('WOOCOMMERCE_PRODUCT_VARIATIONS_1500', cast=int)
+    2500: config('WOOCOMMERCE_PRODUCT_VARIATIONS_2500', cast=int)
 }
 
 
@@ -56,9 +56,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Email configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'adds.lcs.server@gmail.com'
+EMAIL_HOST_PASSWORD = 'fimshohmujrxosyg'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'webd25479@gmail.com'
-EMAIL_HOST_PASSWORD = 'xfzhpownsjzjxhhr'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
