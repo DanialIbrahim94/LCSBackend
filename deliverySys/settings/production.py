04daad5ad724b2fcdd6ff061ba5a4a2b08725ec2
Araari adem <araariadem0@gmail.com>
2023-03-21 18:01:43 +0100

    add production configurations

--- deliverySys/settings/production.py
+++ deliverySys/settings/production.py
@@ -1,40 +1,50 @@
-import os
 from .base import *
+from decouple import config, Csv
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG').upper() == 'TRUE'
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default=[])
 
 
 # WooCommerce
-WOOCOMMERCE_URL = os.environ.get('WOOCOMMERCE_URL')
-WOOCOMMERCE_CONSUMER_KEY = os.environ.get('WOOCOMMERCE_CONSUMER_KEY')
-WOOCOMMERCE_CONSUMER_SECRET = os.environ.get('WOOCOMMERCE_CONSUMER_SECRET')
-WOOCOMMERCE_VERSION = os.environ.get('WOOCOMMERCE_VERSION')
-WOOCOMMERCE_PRODUCT_ID = os.environ.get('WOOCOMMERCE_PRODUCT_ID')
-WOOCOMMERCE_PAYMENT_METHOD = os.environ.get('WOOCOMMERCE_PAYMENT_METHOD')
-WOOCOMMERCE_PAYMENT_METHOD_TITLE = os.environ.get('WOOCOMMERCE_PAYMENT_METHOD_TITLE')
+WOOCOMMERCE_URL = config('WOOCOMMERCE_URL')
+WOOCOMMERCE_CONSUMER_KEY = config('WOOCOMMERCE_CONSUMER_KEY')
+WOOCOMMERCE_CONSUMER_SECRET = config('WOOCOMMERCE_CONSUMER_SECRET')
+WOOCOMMERCE_VERSION = config('WOOCOMMERCE_VERSION', 'wc/v3')
+
+WOOCOMMERCE_PRODUCT_ID = config('WOOCOMMERCE_PRODUCT_ID')
+WOOCOMMERCE_PAYMENT_METHOD = config('WOOCOMMERCE_PAYMENT_METHOD')
+WOOCOMMERCE_PAYMENT_METHOD_TITLE = config('WOOCOMMERCE_PAYMENT_METHOD_TITLE')
 WOOCOMMERCE_PRODUCT_VARIATIONS = {
-    100: os.environ('WOOCOMMERCE_PRODUCT_VARIATIONS_100'),
-    250: os.environ('WOOCOMMERCE_PRODUCT_VARIATIONS_250'),
-    500: os.environ('WOOCOMMERCE_PRODUCT_VARIATIONS_500'),
-    1000: os.environ('WOOCOMMERCE_PRODUCT_VARIATIONS_1000'),
-    2500: os.environ('WOOCOMMERCE_PRODUCT_VARIATIONS_2500')
+    100: config('WOOCOMMERCE_PRODUCT_VARIATIONS_100', cast=int),
+    250: config('WOOCOMMERCE_PRODUCT_VARIATIONS_250', cast=int),
+    500: config('WOOCOMMERCE_PRODUCT_VARIATIONS_500', cast=int),
+    1000: config('WOOCOMMERCE_PRODUCT_VARIATIONS_1000', cast=int),
+    2500: config('WOOCOMMERCE_PRODUCT_VARIATIONS_2500', cast=int)
 }
 
 
 # Coupons
 MINIMUM_COUPONS_AMOUNT = 40
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT')
+    }
 }
 
 
@@ -62,11 +72,17 @@ CORS_ORIGIN_WHITELIST = []
 
 # Email configuration
 
-EMAIL_BACKEND = ''
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 0
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = ''
-RECIPIENT_ADDRESS = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer'
+    ]
+}
