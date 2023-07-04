b206d11a64ffb6175e02ad4bc3157f253806b0e2
Araari adem <araariadem0@gmail.com>
2023-07-04 03:20:58 +0200

    add warning header at the start of the form

--- deliverySys/settings/base.py
+++ deliverySys/settings/base.py
@@ -108,4 +108,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Define the time interval, in minutes, for processing new submissions
-SUBMISSION_PROCESSING_TIME_WINDOW = 5
+SUBMISSION_PROCESSING_TIME_WINDOW = 1
